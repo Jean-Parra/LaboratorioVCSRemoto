ff8b318957a7868fd0d1ffc42e4c08de4570cbbd
Jean-Parra <62663769+Jean-Parra@users.noreply.github.com>
2020-03-26 13:51:05 -0500

    Mi primer commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# punto 5
+
+a = float(input("ingrese el valor del primer numero: "))
+c = float(input("ingrese el valor del segundo numero: "))
+
+print ("el producto de los dos numeros es: " + str(a+c))
+print ("el doble del segundo numero es: " + str(a*2))
