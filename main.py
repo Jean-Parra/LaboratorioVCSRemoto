b81aa33769ffb37aab737ab739a472667a1e186b
Jean-Parra <62663769+Jean-Parra@users.noreply.github.com>
2020-03-26 22:58:38 -0500

    ultima modificacion

--- main.py
+++ main.py
@@ -5,21 +5,38 @@ Spyder Editor
 This is a temporary script file.
 """
 
-# punto 12
+# empresa de transporte
 
-a  =  float ( input ( "ingrese el valor del primer numero: " ))
-b  =  float ( input ( "ingrese el valor del segundo numero: " ))
-c  =  float ( input ( "ingrese el valor del tercer numero: " ))
-
-d  =  b ** 2  - 4 * a * c
-e  =  2 * a 
-
-if  d  >  0 :
-    print ( "x1 =" , ( - b  +  d ** 0.5 ) / e )
-    print ( "x2 =" , ( - b  -  d ** 0.5 ) / e )
-
-elif  d  ==  0 :
-        print ( "x1 = x2 =" , - b / e )
-
-elif  d  <  0 :
-         print ( "no exixte solucion de la ecuacion cuadratica en el dominio de los reales" )
+PesoAcumulado = 0
+D = float(input("ingrese la distancia: "))
+L = 64000
+PP = 4500
+PK = 4000
+PM = 8000
+VN = 1
+VI = 2
+while PesoAcumulado <= L *0.95 :
+    P = float(input("ingrese el peso: "))
+    if P > 10:
+        PesoAcumulado = PesoAcumulado + P
+        if PesoAcumulado <= L:
+            if VN == 1:
+                ValorDistancia = PK * D
+                ValorPeso = PP * P
+                ValorTotal = ValorDistancia + ValorPeso
+                print("el valor total es: " + str (ValorDistancia + ValorPeso))
+                if P > 100 :
+                    print("el valor total es: " + str (ValorTotal * 0.85))
+            elif VI == 2:
+                ValorDistancia = PM * D
+                ValorPeso = PP * P
+                print("el valor total es: " + str (ValorDistancia + ValorPeso))
+                if P >400 and D >8000:
+                    print("el valor total es: " + str (ValorTotal * 0.90))
+        else:
+            print("el paquete supera el limite")
+            PesoAcumulado = PesoAcumulado - P
+    else: 
+        print("peso no aceptable")
+    if PesoAcumulado >= L * 0.95 and L <= 355000 :
+        print("el peso del avion es: " + str(PesoAcumulado))
