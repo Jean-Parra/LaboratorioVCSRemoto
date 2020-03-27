6fd7e26ddcd133f3f995c8a8914ddb086fa92b4d
Jean-Parra <62663769+Jean-Parra@users.noreply.github.com>
2020-03-26 22:36:42 -0500

    solucion simple terminada

--- main.py
+++ main.py
@@ -5,11 +5,21 @@ Spyder Editor
 This is a temporary script file.
 """
 
-# punto 5
+# punto 12
 
-a = int(input("introduzca el primer numero: "))
-c = int(input("introduzca el segundo numero: "))
+a  =  float ( input ( "ingrese el valor del primer numero: " ))
+b  =  float ( input ( "ingrese el valor del segundo numero: " ))
+c  =  float ( input ( "ingrese el valor del tercer numero: " ))
 
-print("el producto del primer numero y el segundo es: " + str(a*c))
-print("el doble del primer numero es: " + str(a*2))
+d  =  b ** 2  - 4 * a * c
+e  =  2 * a 
 
+if  d  >  0 :
+    print ( "x1 =" , ( - b  +  d ** 0.5 ) / e )
+    print ( "x2 =" , ( - b  -  d ** 0.5 ) / e )
+
+elif  d  ==  0 :
+        print ( "x1 = x2 =" , - b / e )
+
+elif  d  <  0 :
+         print ( "no exixte solucion de la ecuacion cuadratica en el dominio de los reales" )
