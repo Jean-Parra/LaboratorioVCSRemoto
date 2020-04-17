0c1f01ac83bad1105a8afcf48446b9e55ab50673
Jean-Parra <62663769+Jean-Parra@users.noreply.github.com>
2020-04-16 21:04:03 -0500

    PreInforme11_JeanParra

---
+++ PreInforme11_JeanParra.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 16 18:35:13 2020
+
+@author: Jean Parra 
+"""
+
+import numpy as np
+
+def numeros():
+    cantidad = np.array([[85,54,26,74,26,85,74,64,84,23,74],
+                         [57,37,25,84,85,24,86,73,58,42,32],
+                         [74,58,25,85,24,95,24,58,13,76,65]])
+    return cantidad
+
+def Orden(cantidad):
+   Ordenanza = np.sort(cantidad)[::-1]
+   return Ordenanza
+
+
+def cantidadnumeros(numeros):
+    total = cantidad.size
+    return total
+
+def Shape(numeros):
+    cantidad.shape
+    
+
+cantidad = numeros()
